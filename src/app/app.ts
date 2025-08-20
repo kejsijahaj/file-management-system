e743da9c7c8c2d797496faef868b5323a15f4fca
keth <kejsijahaj95@gmail.com>
2025-08-20 11:17:05 +0200

    ui components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./ui/header/header";
+import { Sidenav } from "./ui/sidenav/sidenav";
+import { Content } from "./ui/content/content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Sidenav, Content],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
