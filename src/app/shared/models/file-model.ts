16a2b9f0a4d0d4c981dee74878c0aedb156ff461
keth <kejsijahaj95@gmail.com>
2025-08-20 11:15:46 +0200

    models

---
+++ src/app/shared/models/file-model.ts
@@ -0,0 +1,11 @@
+export interface FileItem {
+    id: number;
+    userId: number;
+    folderId: number;
+    name: string;
+    mime: string;
+    size: number;
+    url: string;
+    createdAt: string;
+    updatedAt: string;
+}
---
+++ src/app/shared/models/folder-model.ts
@@ -0,0 +1,7 @@
+export interface Folder {
+    id: number;
+    userId: number;
+    name: string;
+    parentId: number;
+    createdAt: string;
+}
