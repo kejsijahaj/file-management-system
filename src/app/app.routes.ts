2f10cefc04c9a6faf1404bd736a0f7f84bad22a9
keth <kejsijahaj95@gmail.com>
2025-08-20 14:21:13 +0200

    route conf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Content } from './ui/content/content';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'drive/folder/0'},
+    { path: 'drive/folder/:id', component: Content},
+    { path: '**', redirectTo: 'drive/folder/0'}
+];
