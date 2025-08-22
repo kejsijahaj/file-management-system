1fc0a94a17dd1824e813d782d48910a031d72c7e
keth <kejsijahaj95@gmail.com>
2025-08-22 11:26:26 +0200

    app+route changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Content } from './ui/content/content';
+import { Login } from './ui/login/login';
+import { Register } from './ui/register/register';
 
 export const routes: Routes = [
-    {path: '', pathMatch: 'full', redirectTo: 'drive/folder/0'},
-    { path: 'drive/folder/:id', component: Content},
-    { path: '**', redirectTo: 'drive/folder/0'}
+    {path: '', pathMatch: 'full', redirectTo: 'login'},
+    // { path: 'drive/folder/:id', component: Content},
+    // { path: '**', redirectTo: 'drive/folder/0'}, // no data found component
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
 ];
