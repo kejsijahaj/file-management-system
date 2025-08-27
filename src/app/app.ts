91b6520db7cb5bc788854c7f55f24e96c16ef33f
keth <kejsijahaj95@gmail.com>
2025-08-27 14:57:51 +0200

    draft changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./ui/header/header";
 import { Sidenav } from "./ui/sidenav/sidenav";
 import { Content } from "./ui/content/content";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidenav, Content],
+  imports: [RouterOutlet, Header, Sidenav, Content, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/ui/sidenav/sidenav.ts
+++ src/app/ui/sidenav/sidenav.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, linkedSignal, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { TrackByFunction } from '@angular/core';
 import { MatTreeModule } from '@angular/material/tree';
@@ -8,6 +8,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { ApiService } from '../../core/api/api-service';
 import { DriveStore } from '../../features/drive/state/drive-store';
+import { BehaviorSubject, forkJoin, switchMap } from 'rxjs';
+import {toSignal} from "@angular/core/rxjs-interop"
 
 type NodeType = 'folder' | 'file';
 
@@ -33,15 +35,43 @@ export class Sidenav {
   store = inject(DriveStore);
   private router = inject(Router);
 
-  data: TreeNode[] = [
+  readonly #getData = new BehaviorSubject<void>(undefined);
+  readonly #data$ = this.#getData.asObservable().pipe(switchMap(() => {
+    return forkJoin([
+        this.api.listChildrenFolders(this.store.userId(), 0),
+        this.api.listFilesInFolder(this.store.userId(), 0),
+      ])
+  }))
+  readonly #data = toSignal(this.#data$)
+
+  private expandedIds = signal(new Set<number>([0]));
+
+
+  protected readonly data = linkedSignal<TreeNode[]>(() => {
+    const folders: TreeNode[] = (this.#data()?.[0] ?? []).map(f => ({id: f.id,
+        name: f.name,
+        type: 'folder' as const,
+        children: <TreeNode[]>[],
+        loaded: false,}))
+    const files: TreeNode[] = (this.#data()?.[1] ?? []).map(f => ({
+          id: f.id,
+          name: f.name,
+          type: 'file' as const,
+          mime: f.mime,
+        }))
+    
+
+    return [
     {
       id: 0,
       name: 'My Drive',
       type: 'folder',
-      children: [],
-      loaded: false,
+      children: [...folders, ...files],
+              // loaded: false,
     },
-  ];
+  ]
+  });
+  nodesEff = effect(() => console.log(`nodes chnged ${JSON.stringify(this.data())}`))
 
   childrenOf = (n: TreeNode) => (n.type === 'folder' ? n.children ?? [] : []);
   trackByNode: TrackByFunction<TreeNode> = (_i, node) => `${node.type}:${node.id}`;
@@ -49,74 +79,37 @@ export class Sidenav {
   isFolder = (_: number, n: TreeNode) => n.type === 'folder';
   isFile = (_: number, n: TreeNode) => n.type === 'file';
 
-  private expandedIds = signal(new Set<number>());
   expanded = (n: TreeNode) => this.expandedIds().has(n.id);
 
 
   ngOnInit() {
-    this.loadChildren(this.data[0]);
-    this.toggle(this.data[0], true);
+    // this.loadChildren(this.data()[0]);
+    // this.toggle(this.data()[0], true);
   }
 
-  async toggle(node: TreeNode, forceOpen?: boolean) {
-    if (node.type !== 'folder') return;
+  toggle(node: TreeNode, forceOpen?: boolean) {
+    // if (node.type !== 'folder') return;
 
-    const next = new Set(this.expandedIds());
-    const wantOpen = forceOpen ?? !next.has(node.id);
+    // const next = new Set(this.expandedIds());
+    // const wantOpen = forceOpen ?? !next.has(node.id);
 
-    if (wantOpen && !node.loaded) {
-      await this.loadChildren(node);
-    }
+    // if (wantOpen && !node.loaded) {
+    //   // await this.loadChildren(node);
+    //   this.#getData.next()
+    // }
 
-    if (wantOpen) next.add(node.id);
-    else next.delete(node.id);
+    // if (wantOpen) next.add(node.id);
+    // else next.delete(node.id);
 
-    this.expandedIds.set(next);
-    this.refresh();
-  }
+    // this.expandedIds.set(next);
+    // // this.refresh();
+    // this.data.update(n => [...n])
 
-  async loadChildren(node: TreeNode) {
-    node.loading = true;
-    this.refresh();
-    try {
-      const uid = this.store.userId();
-      const [folders, files] = await Promise.all([
-        this.api.listChildrenFolders(uid, node.id),
-        this.api.listFilesInFolder(uid, node.id),
-      ]);
-
-      console.log({folders})
-
-      // console.table(folders, ['id', 'name']);
-      // console.table(files, ['id', 'name', 'mime']);
-
-      console.log({node})
-      node.children = [
-        ...folders.map((f) => ({
-          id: f.id,
-          name: f.name,
-          type: 'folder' as const,
-          children: [],
-          loaded: false,
-        })),
-        ...files.map((f) => ({
-          id: f.id,
-          name: f.name,
-          type: 'file' as const,
-          mime: f.mime,
-          
-        })),
-      ];
-
-      console.log('Assigned children for node', node.id, node.children);
-      node.loaded = true;
-    } catch (e) {
-      console.error('loadChildren failed for node', node.id, e);
-      node.children ??= [];
-    } finally {
-      node.loading = false;
-      this.refresh();
-    }
+      if (node.type !== 'folder') return;
+  const next = new Set(this.expandedIds());
+  if (next.has(node.id)) next.delete(node.id);
+  else next.add(node.id);
+  this.expandedIds.set(next);
   }
 
   async open(node: TreeNode) {
@@ -128,7 +121,7 @@ export class Sidenav {
     }
   }
 
-  private refresh() {
-    this.data = [...this.data];
-  }
+  // private refresh() {
+  //   // this.data() = [...this.data()];
+  // }
 }
