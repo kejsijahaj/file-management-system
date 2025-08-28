787797065200a7569d0760591ae743be3d33a34b
keth <kejsijahaj95@gmail.com>
2025-08-28 12:01:55 +0200

    refactor(app): remove unused components and clean up app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./ui/header/header";
-import { Sidenav } from "./ui/sidenav/sidenav";
-import { Content } from "./ui/content/content";
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,7 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidenav, Content, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/ui/home/home.ts
+++ src/app/ui/home/home.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Sidenav} from '../sidenav/sidenav';
 import { Content } from '../content/content';
+import { MatDrawerContainer } from "@angular/material/sidenav";
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home',
-  imports: [Sidenav, Content, Header],
+  imports: [Sidenav, Content, Header, MatDrawerContainer, MatSidenavModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
