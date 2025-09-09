baf0c3cf33b6607390817383eccfb804e24ac582
keth <kejsijahaj95@gmail.com>
2025-09-09 10:17:48 +0200

    rename file/folder added

--- src/app/ui/content/content.ts
+++ src/app/ui/content/content.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs/operators';
+import { firstValueFrom } from 'rxjs';
 
 import { FilePipe } from '../../shared/pipes/file-pipe';
 import { SizePipe } from '../../shared/pipes/size-pipe';
@@ -11,6 +12,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { ConfirmService } from '../../shared/services/confirm-service';
+import { NameDialog } from '../../shared/components/name-dialog/name-dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-content',
@@ -30,6 +33,7 @@ export class Content {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private confirm = inject(ConfirmService);
+  private dialog = inject(MatDialog)
   store = inject(DriveStore);
 
   // listen to id changes
@@ -68,14 +72,35 @@ export class Content {
     }
   }
 
-  onEditFolder(id: number): void {
-    console.log('edit folder', id);
-    // will change folder name
+  async onEditFolder(folder: {id: number; name: string}) {
+    const ref = this.dialog.open(NameDialog, {
+      width: '400px',
+      data: {
+        title: 'Rename Folder',
+        label: 'New Name',
+        placeholder: folder.name,
+        confirm: 'Rename'
+      }
+    });
+    const newName: string | undefined = await firstValueFrom(ref.afterClosed());
+    if (!newName || newName.trim() === '' || newName === folder.name) return;
+    await this.store.renameFolder(folder.id, newName.trim());
   }
 
-  onEditFile(id: number, name: string): void {
-    console.log('edit file', id, name);
-    // will change file name
+  async onEditFile(file: {id: number; name: string}) {
+    const ref = this.dialog.open(NameDialog, {
+      width: '400px',
+      data: {
+        title: 'Rename File',
+        label: 'New Name',
+        placeholder: file.name,
+        confirm: 'Rename'
+      }
+    });
+    const newName: string | undefined = await firstValueFrom(ref.afterClosed());
+    if (!newName || newName.trim() === '' || newName === file.name) return;
+
+    await this.store.renameFile(file.id, newName.trim());
   }
 
   go(id: number) {
